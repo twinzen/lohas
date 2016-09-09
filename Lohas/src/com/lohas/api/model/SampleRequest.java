1ea140dd2e2fcd0e075a764f0074185aeff8d562
Twinsen Lai <twinzen@gmail.com>
2016-09-09 23:04:46 +0800

    Basic with sample stuff

---
+++ Lohas/src/com/lohas/api/model/SampleRequest.java
@@ -0,0 +1,25 @@
+package com.lohas.api.model;
+
+public class SampleRequest {
+	
+	private String bankName;
+	
+	private String bankType;
+
+	public String getBankType() {
+		return bankType;
+	}
+
+	public void setBankType(String bankType) {
+		this.bankType = bankType;
+	}
+
+	public String getBankName() {
+		return bankName;
+	}
+
+	public void setBankName(String bankName) {
+		this.bankName = bankName;
+	}
+
+}
---
+++ Lohas/src/com/lohas/api/model/SampleResponse.java
@@ -0,0 +1,15 @@
+package com.lohas.api.model;
+
+public class SampleResponse {
+
+	String resultCode;
+
+	public String getResultCode() {
+		return resultCode;
+	}
+
+	public void setResultCode(String resultCode) {
+		this.resultCode = resultCode;
+	}
+	
+}
